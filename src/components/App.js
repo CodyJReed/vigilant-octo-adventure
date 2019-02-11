27b598e3c1599ba58d360ecde76efe771cef7991
Cody Reed <cody2j2reed@gmail.com>
2019-02-11 15:40:38 -0500

    Added react-router-dom, and created routes in app.js

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="ui container">
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
